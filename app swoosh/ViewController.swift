79f45322d8421d0739914b38a97c25e00c88b6f1
VickyPhan <VickyhPhan@gmail.com>
2019-11-20 08:33:46 -0800

    Seagues and Unwind

--- app swoosh/ViewController.swift
+++ app swoosh/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
-
-
+    
+    
 }
 
